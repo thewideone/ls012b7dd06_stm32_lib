583b51cf724f21e72748c3bc71f15766c8af536e
thewideone <szymon.kajda@onet.pl>
2025-02-17 13:51:22 +0100

    Removed unnecessary files

--- ls012b7dd06.h
+++ ls012b7dd06.h
@@ -8,9 +8,6 @@
 #ifndef LS012B7DD06_STM32_INC_LS012B7DD06_H_
 #define LS012B7DD06_STM32_INC_LS012B7DD06_H_
 
-#include <ls012b7dd06_dma.h~>
-#include <ls012b7dd06_ospi.h~>
-#include <ls012b7dd06_tim.h~>
 #include "stm32u5xx_hal.h"
 #include "ls012b7dd06_config.h"
 #include <stdbool.h>
